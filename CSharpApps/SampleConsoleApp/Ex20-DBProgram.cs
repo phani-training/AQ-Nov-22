f70d6193eb361d8a933ab94041c75fb41ad5deb6
Phaniraj <79626160+phani-training@users.noreply.github.com>
2022-11-17 11:47:59 +0530

    Add files via upload

--- CSharpApps/SampleConsoleApp/Ex20-DBProgram.cs
+++ CSharpApps/SampleConsoleApp/Ex20-DBProgram.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using SampleConsoleApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 
 /*
@@ -21,7 +24,55 @@ namespace SampleConsoleApp
     {
         #region ALLCONSTANTS
         const string CONNECTIONSTRING = @"Data Source=.\SQLEXPRESS;Initial Catalog=AQInsightsDB;Integrated Security=True";
+
+        const string UPDATE_QUERY = "UPDATE EMPTABLE SET EmpName = @name, EmpAddress = @address, EmpSalary = @salary where Id = @id";
+
+        const string DELETE_QUERY = "DeleteEmployee"; //Stored Proc Name....
+
+        const string SELECT_QUERY = "SELECT * FROM EMPTABLE";
         #endregion
+
+
+        static Employee[] getAllEmployees()
+        {
+            List<Employee> empList = new List<Employee>();
+            using(SqlConnection con = new SqlConnection(CONNECTIONSTRING))
+            {
+                SqlCommand cmd = new SqlCommand(SELECT_QUERY, con);
+                con.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        var emp = new Employee
+                        {
+                            EmpId = Convert.ToInt32(reader[0]),
+                            EmpName = reader[1].ToString(),
+                            EmpAddress = reader[2].ToString(),
+                            EmpSalary = Convert.ToDouble(reader[3])
+                        };
+                        empList.Add(emp);
+                    }
+                    return empList.ToArray();
+            }
+        }
+        static void deleteRecord(int id)
+        {
+            SqlConnection con = new SqlConnection(CONNECTIONSTRING);
+            SqlCommand cmd = new SqlCommand(DELETE_QUERY, con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@id", id);
+            try
+            {
+                con.Open();
+                var rowsAffecfed = cmd.ExecuteNonQuery();
+                if (rowsAffecfed != 1)
+                    throw new Exception("No Record found to delete");
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
         static void addNewRecord(string name, string address, double salary)
         {
             string insertQuery = $"Insert into EmpTable values('{name}', '{address}', {salary})";
@@ -37,7 +88,7 @@ namespace SampleConsoleApp
             }
             catch (SqlException ex)
             {
-                Console.WriteLine(ex.Message);
+                throw ex;
             }
             finally
             {
@@ -45,9 +96,43 @@ namespace SampleConsoleApp
             }
             //Display a Success message
         }
+
+        static void updateRecord(int id, string name, string address, double salary)
+        {
+            SqlConnection con = new SqlConnection(CONNECTIONSTRING);
+            SqlCommand cmd = new SqlCommand(UPDATE_QUERY, con);
+            cmd.Parameters.AddWithValue("@name", name);
+            cmd.Parameters.AddWithValue("@address", address);
+            cmd.Parameters.AddWithValue("@salary", salary);
+            cmd.Parameters.AddWithValue("@id", id);
+            try
+            {
+                con.Open();
+                int rowsAffected = cmd.ExecuteNonQuery();
+                if (rowsAffected != 1)
+                    throw new Exception("No Record found to update");
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
         static void Main(string[] args)
         {
             addNewRecord("Ramesh", "Hyderabad", 70000);
+            try
+            {
+                var records = getAllEmployees();
+                foreach(var emp in records)
+                    Console.WriteLine(emp);
+                //deleteRecord(4);
+                //updateRecord(4, "Ramesh Reddy", "Panjagutta, Hyderabad", 65000);
+                Console.WriteLine("Record Deleted Succesfully");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
---
+++ CSharpApps/SampleConsoleApp/Ex23-DisconnectedModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SampleConsoleApp
+{
+    class DisconnecedExample
+    {
+        const string CONNECTIONSTRING = @"Data Source=.\SQLEXPRESS;Initial Catalog=AQInsightsDB;Integrated Security=True";
+        static DataTable getAllRecords()
+        {
+            SqlConnection con = new SqlConnection(CONNECTIONSTRING);
+            SqlCommand cmd = new SqlCommand("SELECT * FROM EMPTABLE", con);
+            SqlDataAdapter ada = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet("SomeName");
+            ada.Fill(ds, "TableName");
+            Console.WriteLine("The Current ConnectionState: " + con.State);
+            return ds.Tables["TableName"];
+        }
+        static void Main(string[] args)
+        {
+            var records = getAllRecords();
+            foreach(DataRow row in records.Rows)
+            {
+                Console.WriteLine($"{row[1]} comes from {row[2]}");
+            }
+        }
+    }
+}
